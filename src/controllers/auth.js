54967f602b4404ac9b28566e7043388861f01ee9
Alex Ricardo <alex.ricardo1999@hotmail.com>
2020-06-29 22:47:46 -0300

    Storing account into database.

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,13 +1,28 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const {Account} = require('../models');
 
 const router = express.Router();
+
+const saltRound = 10;
 
 router.get('/sign-in', (req, res) => {
     return res.json('sign-in');
 });
 
-router.get('/sign-out', (req, res) => {
-    return res.json("Sign-up");
+router.get('/sign-up', async(req, res) => {
+
+    const email = "alex@teste.com";
+    const password = "123456";
+
+   
+    const hash = bcrypt.hashSync(password, saltRound)
+
+    console.log(hash);
+
+    const result = await Account.create({email, password: hash}).then().catch();
+
+    return res.json(result);
 });
 
 
